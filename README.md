f09f4974185e13242841f234073be1396b6fd4f2
Derek Bredensteiner <derekbreden@gmail.com>
2024-08-08 04:06:38 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# truce-android
+ 
---
+++ app/src/main/java/net/truce/twa/DelegationService.java
@@ -0,0 +1,14 @@
+package net.truce.twa;
+
+
+
+public class DelegationService extends
+        com.google.androidbrowserhelper.trusted.DelegationService {
+    @Override
+    public void onCreate() {
+        super.onCreate();
+
+        
+    }
+}
+
